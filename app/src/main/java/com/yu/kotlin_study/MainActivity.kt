047f76f0f67f7a6ac919f99738aa699e089c2edc
Mengfei.Yu <Ymf@210119>
2022-05-26 15:09:46 +0800

    支持viewBinding

--- app/src/main/java/com/yu/kotlin_study/MainActivity.kt
+++ app/src/main/java/com/yu/kotlin_study/MainActivity.kt
@@ -2,10 +2,29 @@ package com.yu.kotlin_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.yu.kotlin_study.databinding.ActivityMainBinding
 
+
+/**
+ * 主页面用于跳转其他功能页
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initOnClick()
+    }
+
+    /**
+     * 设置监听
+     */
+    fun initOnClick() {
+        binding.bt1.setOnClickListener { Log.d("MainActivity", "bt1 OnClick") }
     }
 }
