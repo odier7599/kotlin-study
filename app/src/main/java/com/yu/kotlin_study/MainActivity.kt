19303719f7ad6e23d8f0163b6e19f78c1c15a72b
Mengfei.Yu <Ymf@210119>
2022-05-26 14:35:41 +0800

    first commit

---
+++ app/src/main/java/com/yu/kotlin_study/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yu.kotlin_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
